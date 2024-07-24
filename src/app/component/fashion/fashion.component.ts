bed0aa0d388f8ca4047a6a5f38598939035e7079
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-07-24 18:16:55 +0530

    Create fashion.component.ts

---
+++ src/app/component/fashion/fashion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fashion',
+  templateUrl: './fashion.component.html',
+  styleUrls: ['./fashion.component.css']
+})
+export class FashionComponent {
+
+}
